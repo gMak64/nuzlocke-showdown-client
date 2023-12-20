817e13a8859f0fe6a89e2dd3d9ff20a2e96d966e
gMak64 <46451004+gMak64@users.noreply.github.com>
2023-12-20 00:25:41 -0500

    new client first push

--- pokemonshowdown.com/index.php
+++ pokemonshowdown.com/index.php
@@ -86,12 +86,12 @@ include_once __DIR__ . '/../config/ads-landing.inc.php';
 
 	<header>
 		<div class="nav-wrapper"><ul class="nav">
-			<li><a class="button nav-first cur" href="/"><img src="/images/pokemonshowdownbeta.png" srcset="/images/pokemonshowdownbeta.png 1x, /images/pokemonshowdownbeta@2x.png 2x" alt="Pok&eacute;mon Showdown" width="146" height="44" /> Home</a></li>
+			<!-- <li><a class="button nav-first cur" href="/"><img src="/images/pokemonshowdownbeta.png" srcset="/images/pokemonshowdownbeta.png 1x, /images/pokemonshowdownbeta@2x.png 2x" alt="Pok&eacute;mon Showdown" width="146" height="44" /> Home</a></li> -->
 			<li><a class="button" href="/dex/">Pok&eacute;dex</a></li>
 			<li><a class="button" href="//replay.pokemonshowdown.com/">Replays</a></li>
 			<li><a class="button" href="/ladder/">Ladder</a></li>
 			<li><a class="button nav-last" href="/forums/">Forum</a></li>
-			<li><a class="button greenbutton nav-first nav-last" href="//play.pokemonshowdown.com/">Play</a></li>
+			<li><a class="button greenbutton nav-first nav-last" href="/play/">Play</a></li>
 		</ul></div>
 	</header>
 
