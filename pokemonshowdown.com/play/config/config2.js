7e84d0bcb6e59923938b133dc959043d2d9d9b20
gMak64 <46451004+gMak64@users.noreply.github.com>
2024-02-28 16:48:27 -0500

    build

--- pokemonshowdown.com/play/config/config2.js
+++ pokemonshowdown.com/play/config/config2.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (674371df)";
+Config.version = "0.11.2 (ea9e86ae/54e0e4ff)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
