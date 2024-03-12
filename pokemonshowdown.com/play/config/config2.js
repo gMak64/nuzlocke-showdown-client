06d96dbb867628b9306dc4501ba3f199252f878c
gMak64 <46451004+gMak64@users.noreply.github.com>
2024-03-11 20:37:41 -0400

    update moves

--- pokemonshowdown.com/play/config/config2.js
+++ pokemonshowdown.com/play/config/config2.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (7e84d0bc)";
+Config.version = "0.11.2 (c718ef88)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
