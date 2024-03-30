8de0a3162f54e08f46c43c180d0dbf5b50ddbc4b
gMak64 <46451004+gMak64@users.noreply.github.com>
2024-03-30 19:41:01 -0400

    proper display

--- pokemonshowdown.com/play/config/config2.js
+++ pokemonshowdown.com/play/config/config2.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (06d96dbb)";
+Config.version = "0.11.2 (66bdcd4c)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
