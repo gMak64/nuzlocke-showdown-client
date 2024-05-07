2e9cf506dc89d84f472600fc785e1f8ad057cc6c
gMak64 <46451004+gMak64@users.noreply.github.com>
2024-05-06 20:07:08 -0400

    hard code

--- pokemonshowdown.com/play/js/battledata.js
+++ pokemonshowdown.com/play/js/battledata.js
@@ -5750,7 +5750,8 @@ var eepyTemplate='';
 if(this.eepyTerrainUp){
 eepyTemplate=this.template('switchIn','eepyterrain','NODEFAULT').replace('[POKEMON]',this.pokemon(pokemon));
 }
-return eepyTemplate+template.replace('[TRAINER]',this.trainer(_side)).replace('[FULLNAME]',fullname);
+return this.template('switchIn','eepyterrain','NODEFAULT').replace('[POKEMON]',this.pokemon(pokemon));
+
 }
 
 case'drag':{
