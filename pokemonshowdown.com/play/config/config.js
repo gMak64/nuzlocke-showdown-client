27263d6087b7c5b66b55364b0aae143024ce51b5
Grant Mak <grantmak@Grants-MacBook-Pro.local>
2024-08-28 18:11:44 -0400

    aesthetics

--- pokemonshowdown.com/play/config/config.js
+++ pokemonshowdown.com/play/config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (d2df1da6)";
+Config.version = "0.11.2 (9ef9ea01)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
